dbf5cda342b27634d5bcfe5818a2f099b888ade7
theyadoreboy <fahmiastratech@gmail.com>
2025-10-28 09:39:50 +0700

    perbaharuan dependency

--- Template_DevExpress_By_MFM/Template_DevExpress_By_MFM/Scripts/_references.js
+++ Template_DevExpress_By_MFM/Template_DevExpress_By_MFM/Scripts/_references.js
@@ -1,4 +1,4 @@
-﻿/// <reference path="jquery-3.5.1.js" />
+﻿/// <reference path="jquery-3.7.1.js" />
 /// <autosync enabled="true" />
 /// <reference path="dx-gantt.js" />
 /// <reference path="dx-diagram.js" />
