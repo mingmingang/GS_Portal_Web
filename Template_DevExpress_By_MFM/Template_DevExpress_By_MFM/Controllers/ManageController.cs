f00a640bb28a585ac7a0fbfb02930f40fb598314
theyadoreboy <fey.tachikawa@gmail.com>
2025-12-01 13:43:09 +0700

    penyesuaian gstrack fitur cuti

--- Template_DevExpress_By_MFM/Template_DevExpress_By_MFM/Controllers/ManageController.cs
+++ Template_DevExpress_By_MFM/Template_DevExpress_By_MFM/Controllers/ManageController.cs
@@ -893,7 +893,7 @@ namespace Template_DevExpress_By_MFM.Controllers
         }
 
         [SessionCheck]
-        public ActionResult ManageEditCuti(string id)
+        public ActionResult ManageEditCuti(string id, int? sisa = null)
         {
             var session = HttpContext.Session["SHealth"] as SessionLogin;
             if (session == null)
@@ -913,6 +913,26 @@ namespace Template_DevExpress_By_MFM.Controllers
             ViewBag.BackUrl = backUrl;
 
             ViewBag.CutiId = id;
+            ViewBag.SisaCuti = sisa ?? 0;
+
+            return View();
+        }
+
+        [SessionCheck]
+        public ActionResult ManageTambahCuti(string tipe = null, int? sisa = null)
+        {
+            var logSession = Session["SHealth"] as Template_DevExpress_By_MFM.Models.SessionLogin;
+            if (logSession == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+
+            ViewBag.Title = "Tambah Cuti";
+            ViewBag.Npk = logSession.npk;
+            ViewBag.Fullname = logSession.fullname;
+            ViewBag.EmpId = logSession.empid;
+            ViewBag.SelectedTipeCuti = tipe;
+            ViewBag.SisaCuti = sisa ?? 0;
 
             return View();
         }
