2dd6c5fdb97e0dc1336c63c0b0de18ab6805b623
aadean11 <fiqridean@gmail.com>
2026-02-14 09:28:09 +0700

    >

--- Template_DevExpress_By_MFM/Template_DevExpress_By_MFM/Controllers/HomeApiController.cs
+++ Template_DevExpress_By_MFM/Template_DevExpress_By_MFM/Controllers/HomeApiController.cs
@@ -14,8 +14,8 @@ namespace Template_DevExpress_By_MFM.Controllers
     public class HomeApiController : ApiController
     {
         #region Konfigurasi & Properti
-        private const string SunfishApiBaseUrl = "http://10.19.101.146:44320/api/gstracker/home";
-        //private const string SunfishApiBaseUrl = "http://localhost:44320/api/gstracker/home";
+        //private const string SunfishApiBaseUrl = "http://10.19.101.146:44320/api/gstracker/home";
+        private const string SunfishApiBaseUrl = "http://localhost:44320/api/gstracker/home";
 
         private const string SunfishApiClientId = "GSBattery-5+nzLK0woWSZc1JDl9bylDoLx/Hzhs";
         private const string SunfishApiClientSecret = "5+nzLK0woWSZc1JDl9bylDoLx/HzhsmegK2KqWqp67OgoYYYX/ncDpc3VpQAAKhbSeJh1CjkIrms+pDt1UlRZMC985mBXUJ1YYPV";
